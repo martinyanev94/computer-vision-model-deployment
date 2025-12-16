80d0605caf9d96308bea3db27a72d1f7d54d3b4b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 19:16:28 -0500

    Add Lesson_8_Implementation_Of_Neural_Networks_In_Pytorch/part_1_torch_visualization.bash

---
+++ Lesson_8_Implementation_Of_Neural_Networks_In_Pytorch/part_1_torch_visualization.bash
@@ -0,0 +1,8 @@
+pip install torch torchvision matplotlib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torchvision import datasets
+import matplotlib.pyplot as plt
