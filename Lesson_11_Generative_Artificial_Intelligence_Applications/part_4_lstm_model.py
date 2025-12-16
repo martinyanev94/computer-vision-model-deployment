03bf0d6d3c391aad1bc736d4bcd16521d8563393
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 19:13:07 -0500

    Add Lesson_11_Generative_Artificial_Intelligence_Applications/part_4_lstm_model.py

---
+++ Lesson_11_Generative_Artificial_Intelligence_Applications/part_4_lstm_model.py
@@ -0,0 +1,12 @@
+class LSTMModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super(LSTMModel, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x, _ = self.lstm(x)
+        x = self.fc(x[:, -1, :])  # only take the output from the last time step
+        return x
