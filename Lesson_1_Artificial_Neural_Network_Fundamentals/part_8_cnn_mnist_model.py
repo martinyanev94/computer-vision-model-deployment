6ef3e6184038a111853a529d31d51b8b1eb90842
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-15 19:14:15 -0500

    Add Lesson_1_Artificial_Neural_Network_Fundamentals/part_8_cnn_mnist_model.py

---
+++ Lesson_1_Artificial_Neural_Network_Fundamentals/part_8_cnn_mnist_model.py
@@ -0,0 +1,9 @@
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
